e963451dfb9d87cf1a3eb542f99572dc2a97ae23
fal <54687030+fewdw@users.noreply.github.com>
2023-02-24 18:55:22 -0500

    hardcoded

--- mailer.py
+++ mailer.py
@@ -3,8 +3,8 @@ from email.mime.text import MIMEText
 import smtplib
 import os
 
-smtp_email = "frederic.alefebvre@gmail.com"
-smtp_password = "kdqhtwbnesoakare"
+smtp_email = os.environ.get("smtp_email")
+smtp_password = os.environ.get("smtp_password")
 
 def send_mail(email, subject, message):
 
